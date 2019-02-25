3ace53cae2f71ee6d2749f2bcb14a3bdb967730d
Heatwave114 <heatwavemachine@gmail.com>
2019-02-26 00:37:09 +0100

    'electorate2_2019'

--- electorate/presidential/views.py
+++ electorate/presidential/views.py
@@ -8,7 +8,7 @@ def index(request):
     party_dict = { 'now': now,
 
                     'parties': {'Ekiti': dict(APC=219231, PDP=154032), 'Osun': dict(APC=347634, PDP=337377), 'FCT': dict(APC=152224, PDP=259997), 'Kwara': dict(APC=308984, PDP=138184), 'Nassarawa': dict(APC=289903, PDP=283847), 'Ondo': dict(APC=241769, PDP=275901), 'Kogi': dict(APC=285894, PDP=218207), 'Gombe': dict(APC=402961, PDP=138484), 'Abia': dict(APC=85058, PDP=219698), 'Yobe': dict(APC=497914, PDP=50763),
-                                'Enugu': dict(APC=54423, PDP=355553)}
+                                'Enugu': dict(APC=54423, PDP=355553), 'Ebonyi': dict(APC=90726 PDP=258573)}
 
                     }
 
